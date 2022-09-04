ad824727247713a46eb399e31f7f832b85c414fe
postall74 <postall_mcs@mail.ru>
2022-09-05 02:21:30 +0500

    Added Shop class
    Добавлен класс Shop, возможность покупки башен. Добавлен в UI панель с кнопками

--- Assets/Scripsts/Level/Node.cs
+++ Assets/Scripsts/Level/Node.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
@@ -7,27 +8,41 @@ public class Node : MonoBehaviour
     private GameObject _turret;
     private Renderer _renderer;
     private Color _defaultColor;
+    private BuildManager _buildManager;
 
     private void Start()
     {
         _renderer = GetComponent<Renderer>();
         _defaultColor = _renderer.material.color;
+        _buildManager = BuildManager.instance;
     }
 
     private void OnMouseEnter()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+                return;
+
+        if (_buildManager.GetTurretToBuild() == null)
+            return;
+
         _renderer.material.color = _hoverColor;
     }
 
     private void OnMouseDown()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (_buildManager.GetTurretToBuild() == null)
+            return;
+
         if (_turret != null)
         {
             Debug.Log("Can't build there - TODO: Display on screen");
             return;
         }
 
-        GameObject turretToBuild = BuildManager.instance.GetTurretTToBuild();
+        GameObject turretToBuild = _buildManager.GetTurretToBuild();
         _turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
     }
 
