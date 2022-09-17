e7b4eb67095acb6813af66ec1cb8a0fbeae146b6
postall74 <postall_mcs@mail.ru>
2022-09-18 04:13:25 +0500

    Added function UPGRADE tower
    Добавлена возможность улучшения башен. Обновлены классы BuildMamager/Node/NodeUI под обновленные условия.
    1. Из класса BuildManager убрана ответственность за строительство башен, и перенесена в класс Node
    2. В классе Node добавлена ответственность за строительство и улучшение башни. Что является более логичным решением, т.к. нода должна знать что стоит на ней и что с этим можно что-то делать
    3. Обновлен класс NodeUI под текущее расширение. Добавлено управление кнопкой Upgrade и тестом которое она содержит.

--- Assets/Scripsts/GameMaster/BuildManager.cs
+++ Assets/Scripsts/GameMaster/BuildManager.cs
@@ -6,14 +6,18 @@ public class BuildManager : MonoBehaviour
     public static BuildManager instance;
 
     [SerializeField] private GameObject _buildEffect;
+    [SerializeField] private GameObject _upgradeEffect;
     [SerializeField] private NodeUI _nodeUI;
 
     private TurretBlueprint _turretToBuild;
     private Node _selectedNode; 
 
+    public GameObject BuildEffect => _buildEffect;
+    public GameObject UpgradeEffect => _upgradeEffect;
+    public TurretBlueprint TurretToBuild => _turretToBuild;
 
     public bool CanBuild { get { return _turretToBuild != null; } }
-    public bool HasMoney { get { return PlayerStats.Momey >= _turretToBuild.Cost; } }
+    public bool HasMoney { get { return PlayerStats.Momey >= _turretToBuild.BuildCost; } }
 
     public void SelectNode(Node node)
     {
@@ -49,29 +53,5 @@ public class BuildManager : MonoBehaviour
         }
 
         instance = this;
-    }
-
-    public void BuildTurretOn(Node node)
-    {
-        if (PlayerStats.Momey < _turretToBuild.Cost)
-        {
-            Debug.Log("Not enoth money to build that!");
-            return;
-        }
-
-        Payment(_turretToBuild.Cost);
-
-        GameObject turret = (GameObject)Instantiate(_turretToBuild.Prefab, node.GetBuildPosition(), Quaternion.identity);
-        node.SetBuildTurret(turret);
-
-        GameObject effect = Instantiate(_buildEffect, node.GetBuildPosition(), Quaternion.identity);
-        Destroy(effect, 2f);
-
-        Debug.Log($"Turret build! Money left - {PlayerStats.Momey}");
-    }
-
-    private void Payment(int value)
-    {
-        PlayerStats.ChangeMoney(-value);
     }
 }
--- Assets/Scripsts/Level/Node.cs
+++ Assets/Scripsts/Level/Node.cs
@@ -10,15 +10,50 @@ public class Node : MonoBehaviour
     private Renderer _renderer;
     private Color _defaultColor;
     private BuildManager _buildManager;
+    private TurretBlueprint _turretBlueprint;
+    private bool _isUpgrade = false;
 
-    public Vector3 GetBuildPosition()
+    public TurretBlueprint TurretBlueprint => _turretBlueprint;
+    public bool IsUpgrade => _isUpgrade;
+
+    public Vector3 GetBuildPosition() 
     {
-        return transform.position;
+        return transform.position; 
     }
 
     public void SetBuildTurret(GameObject turret)
     {
         _turret = turret;
+    }
+    
+
+    public void UpdateTurret()
+    {
+        if (PlayerStats.Momey < _turretBlueprint.UpgradeCost)
+        {
+            Debug.Log("Not enoth money to upgrade that!");
+            return;
+        }
+
+        if (_isUpgrade)
+        {
+            Debug.Log("");
+            return;
+        }
+
+        Payment(_turretBlueprint.UpgradeCost);
+
+        Destroy(_turret);
+
+        GameObject turret = (GameObject)Instantiate(_turretBlueprint.UpgradePrefab, GetBuildPosition(), Quaternion.identity);
+        SetBuildTurret(turret);
+
+        GameObject effect = Instantiate(_buildManager.UpgradeEffect, GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 2f);
+
+        _isUpgrade = true;
+
+        Debug.Log("Turret Upgrade");
     }
 
     private void Start()
@@ -60,11 +95,35 @@ public class Node : MonoBehaviour
         if (!_buildManager.CanBuild)
             return;
 
-        _buildManager.BuildTurretOn(this);
+        BuildTurret(_buildManager.TurretToBuild);
     }
 
     private void OnMouseExit()
     {
         _renderer.material.color = _defaultColor;
+    }
+
+    private void BuildTurret(TurretBlueprint turretToBuild)
+    {
+        if (PlayerStats.Momey < turretToBuild.BuildCost)
+        {
+            Debug.Log("Not enoth money to build that!");
+            return;
+        }
+
+        Payment(turretToBuild.BuildCost);
+
+        GameObject turret = (GameObject)Instantiate(turretToBuild.BuildPrefab, GetBuildPosition(), Quaternion.identity);
+        SetBuildTurret(turret);
+
+        _turretBlueprint = turretToBuild;
+
+        GameObject effect = Instantiate(_buildManager.BuildEffect, GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 2f);
+    }
+
+    private void Payment(int value)
+    {
+        PlayerStats.ChangeMoney(-value);
     }
 }
