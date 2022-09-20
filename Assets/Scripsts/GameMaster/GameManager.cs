6d3297dc157963c523285321884f3559b6bf5143
postall74 <postall_mcs@mail.ru>
2022-09-21 03:08:53 +0500

    Added mothid Unlock Levels
    Добавлены методы разблокировки уровней.
    1. На сцене выбора уровней сделана блокировка еще не открытых уровней
    2. Уровни открываются только после прохождения предыдущего уровня

--- Assets/Scripsts/GameMaster/GameManager.cs
+++ Assets/Scripsts/GameMaster/GameManager.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
+using UnityEngine.Device;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private GameObject _gameOverUI;
+    [SerializeField] private string _nextLevel;
+    [SerializeField] private int _levelToUnlock;
+    [SerializeField] private SceneFader _sceneFader;
 
     private static bool _isGameOver;
 
     public static bool IsGameOver => _isGameOver;
+
+    public void WinLevel()
+    {
+        Debug.Log("Level WON!");
+        PlayerPrefs.SetInt("levelReached", _levelToUnlock);
+        _sceneFader.FadeTo(_nextLevel);
+    }
 
     private void Start()
     {
--- Assets/Scripsts/UI/LevelSelector.cs
+++ Assets/Scripsts/UI/LevelSelector.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
     [SerializeField] private SceneFader _sceneFader;
+    [SerializeField] private Button[] _levelButtons;
 
     public void Select(string levelName)
     {
@@ -11,7 +13,14 @@ public class LevelSelector : MonoBehaviour
 
     private void Start()
     {
-        
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+
+        for (int i = 0; i < _levelButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+                _levelButtons[i].interactable = false;
+
+        }
     }
 
     private void Update()
